746f1e118f7b1640b1bc433137bf5254dd453ced
jmgmayer <47235096+jmgmayer@users.noreply.github.com>
2019-02-15 17:17:56 -0600

    Add Seed DB Shop

---
+++ Shop.Web/Data/SeedDB.cs
@@ -0,0 +1,44 @@
+﻿namespace Shop.Web.Data
+{
+    using Shop.Web.Data.Entities;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    public class SeedDB
+    {
+        private readonly DataContext context;
+        private Random random;
+
+        public SeedDB(DataContext context)
+        {
+            this.context = context;
+            this.random = new Random();
+        }
+
+        public async Task SeedAsync()
+        {
+            await this.context.Database.EnsureCreatedAsync();
+
+            if (!this.context.Products.Any())
+            {
+                this.AddProduct("iPhone");
+                this.AddProduct("Magic Mouse");
+                this.AddProduct("iWatch Series 4");
+                await this.context.SaveChangesAsync();
+            }
+        }
+
+        private void AddProduct(string name)
+        {
+            this.context.Products.Add(new Product
+            {
+                Name = name,
+                Price = this.random.Next(1000),
+                IsAvailabe = true,
+                Stock = this.random.Next(100)
+            });
+        }
+
+    }
+}
