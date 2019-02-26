b5662af0ffaa1e16b30e2690a96c266a27963b9b
jmgmayer <47235096+jmgmayer@users.noreply.github.com>
2019-02-26 17:35:31 -0600

    Add Login View Model App - Movil

---
+++ Shop.UIForms/Shop.UIForms/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,17 @@
+﻿namespace Shop.UIForms.Infrastructure
+{
+    using Shop.UIForms.ViewModels;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class InstanceLocator
+    {
+        public MainViewModel Main { get; set; }
+
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+    }
+}
---
+++ Shop.UIForms/Shop.UIForms/ViewModels/LoginViewModel.cs
@@ -0,0 +1,42 @@
+﻿namespace Shop.UIForms.ViewModels
+{
+    using GalaSoft.MvvmLight.Command;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Windows.Input;
+    using Xamarin.Forms;
+
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public ICommand LoginCommand => new RelayCommand(Login);
+
+        public LoginViewModel()
+        {
+            this.Email = "rufus170608@gmail.com";
+            this.Password = "rufus170608.";
+        }
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "You must enter a Email", "Accept");
+                return;
+            }
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error","You must enter a Password","Accept");
+                return;
+            }
+            if (!this.Email.Equals("rufus170608@gmail.com") || !this.Password.Equals("rufus170608."))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error","User or password wrong","Accept");
+            }
+
+            await Application.Current.MainPage.DisplayAlert("Ok", "You have logged in to the application", "Accept");
+        }
+    }
+}
